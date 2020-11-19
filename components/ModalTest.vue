c39d4a5752b1190181d8066b31f598f358191fce
BearBaikal <bearbaikal@gmail.com>
2020-11-19 15:34:48 +0300

    Текущее состояние

--- components/ModalTest.vue
+++ components/ModalTest.vue
@@ -1,39 +1,39 @@
 <template>
-  <div class="modal fade" id="test_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal fade" id="test_modal" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-body">
-					<div id="gotest">
-						<div class="zagolovok">Тест на созависимость</div>
-						<div class="opisanie">
+					<!--Next block: Questions-->
+					<div id="questions">
+						<h1>Тест на созависимость</h1>
+						<div class="description">
 							Вам предлагается 20 утверждений, которые нужно оценить применительно к себе.
-							<br/>Отвечайте, не думая слишком долго над каждым утверждением, обычно первый ответ, который приходит в голову, является самым точным.
+							<br/><br/>Отвечайте, не думая слишком долго над каждым утверждением. Обычно первый ответ, который приходит в голову, является самым точным.
 						</div>
-						<div>
-							<ol class="test" v-for="(question, index) in questions" type="1" style="list-style-type:none;padding-left:15px;">
-								<li>{{ index+1 }}. {{ question.content }}
-								<br/>
-								<div class="quest_area">
-									<input type="radio" @change="set_answer(index+1, 1)" :name="'question_' + (index + 1)" style="margin-left:18px;"> 1 (никогда)<br/>
-									<input type="radio" @change="set_answer(index+1, 2)" :name="'question_' + (index + 1)" style="margin-left:18px;"> 2 (иногда)<br/>
-									<input type="radio" @change="set_answer(index+1, 3)" :name="'question_' + (index + 1)" style="margin-left:18px;"> 3 (часто)<br/>
-									<input type="radio" @change="set_answer(index+1, 4)" :name="'question_' + (index + 1)" style="margin-left:18px;"> 4 (почти всегда)
+						<ol class="test" v-for="(question, index) in questions" type="1" style="list-style-type:none;padding-left:15px;">
+							<li>
+								{{ index+1 }}. {{ question.content }}
+								<div class="answer-variants">
+									<input type="radio" @change="set_answer(index + 1, 1)" :name="'question_' + (index + 1)" style="margin-left:18px;"> 1 (никогда)<br/>
+									<input type="radio" @change="set_answer(index + 1, 2)" :name="'question_' + (index + 1)" style="margin-left:18px;"> 2 (иногда)<br/>
+									<input type="radio" @change="set_answer(index + 1, 3)" :name="'question_' + (index + 1)" style="margin-left:18px;"> 3 (часто)<br/>
+									<input type="radio" @change="set_answer(index + 1, 4)" :name="'question_' + (index + 1)" style="margin-left:18px;"> 4 (почти всегда)
 								</div>
-								</li>		
-							</ol>
-							<input type="submit" value="Получить результат" id="show_result" @click="show_result" class="t-submit">
-						</div>
-						<div id="show_answer">
-							<h1>Рузультат теста</h1>
-							Ваш результат: <b id="sum_result"></b> баллов<br/><br/>
-							Шкала оценки результатов:<br/>
-							<ul class='shkala'>
-								<li style='margin-top:5px;'>Более 60 баллов - очень высокая степень зависимых моделей</li>
-								<li style='margin-top:5px;'>От 40 до 59 баллов - высокая степень зависимых моделей</li>
-								<li style='margin-top:5px;'>От 30 до 39 баллов - средняя степень зависимых и/или контрзависимых моделей</li>
-								<li style='margin-top:5px;'>От 21 до 29 баллов - очень мало зависимых и/или высокая степень контрзависимых моделей</li>
-							</ul>
-						</div>
+							</li>		
+						</ol>
+						<input type="submit" value="Получить результат" id="show_result" @click="show_result" class="t-submit">
+					</div>
+					<!--Next block: Answers-->
+					<div id="answers">
+						<h1>Рузультаты теста</h1>
+						<br><span class="result">Ваш результат:</span> <b id="sum_result"></b><br/><br/>
+						Шкала оценки результатов:<br/>
+						<ul class='shkala'>
+							<li style='margin-top:5px;'>Более 60 баллов - очень высокая степень зависимых моделей</li>
+							<li style='margin-top:5px;'>От 40 до 59 баллов - высокая степень зависимых моделей</li>
+							<li style='margin-top:5px;'>От 30 до 39 баллов - средняя степень зависимых и/или контрзависимых моделей</li>
+							<li style='margin-top:5px;'>От 21 до 29 баллов - очень мало зависимых и/или высокая степень контрзависимых моделей</li>
+						</ul>
 					</div>
         </div>
         <div class="modal-footer">
@@ -48,7 +48,7 @@
 	  data: () => ({
 			questions: [
 				//1-5
-				/*{content: "Я склонен (склонна) брать на себя ответственность за чувства и/или поведение других людей"},
+				{content: "Я склонен (склонна) брать на себя ответственность за чувства и/или поведение других людей"},
 				{content: "Я затрудняюсь идентифицировать свои чувства, такие как счастье,злость, смущение, уныние или возбуждение"},
 				{content: "Мне тяжело выражать свои чувства"},
 				{content: "Я испытываю страх или беспокойство при мысли о том, как другие отреагируют на мои чувства или поведение"},
@@ -65,7 +65,7 @@
 				{content: "Мое ощущение собственного достоинства идет извне, в зависимости от мнения или действий других людей, которые, как мне кажется, больше в этом разбираются"},
 				{content: "Я нахожу, что тяжело быть уязвимым (уязвимой) и просить о помощи"},
 				{content: "Я всегда подвергаюсь контролю или стремлюсь контролировать, и наоборот, всегда слежу за тем, чтобы никогда не оказаться ответственным (ответственной)"},
-				//16-20*/
+				//16-20
 				{content: "Я слишком лоялен (лояльна) к другим, даже в том случае, когда эта лояльность не оправдывается"},
 				{content: "У меня привычка рассматривать ситуации по принципу 'все или ничего'"},
 				{content: "Я очень толерантен (толерантна) к непоследовательности и смешанным поручениям"},
@@ -73,7 +73,6 @@
 				{content: "Я стараюсь искать взаимоотношения там, где чувствую себя “нужным” (“нужной”), и пытаюсь затем сохранять их"}
 			],
 			answers: [],
-			show_answers: false,
 			sum: 0
 	  }),
 	  created: function() {
@@ -90,26 +89,29 @@
 					sum += item
 				})
 				document.getElementById("sum_result").innerHTML = sum
-				document.getElementById("show_answer").style.display = "block"
+				document.getElementById("answers").style.display = "block"
 				document.getElementById("show_result").style.display = "none"
+				document.getElementById("questions").style.display = "none"
 				$('#test_modal').animate({ scrollTop: $('#test_modal .modal-content').height() }, 'slow');
 				console.log(sum)
 			},
 			close_test: function(){
-				document.getElementById("sum_result").innerHTML = 0
-				document.getElementById("show_answer").style.display = "none"
-				document.getElementById("show_result").style.display = "block"
+				//document.getElementById("sum_result").innerHTML = 0
+				//document.getElementById("answers").style.display = "none"
+				//document.getElementById("show_result").style.display = "block"
 			}
 	  }
 	}
 </script>
 <style scoped>
-	h1{font-family:Tahoma !important;font-size:1.5rem;font-weight:bold;}
-	#show_answer{display:none;}
+	h1{font-size:2.4rem;font-weight:bold;text-align:center;}
 	.modal-dialog{max-width:1000px;}
-	.quest_area {padding:5px 0 15px 0;}
-	#gotest {padding:20px;font-size:12pt;font-family:Tahoma;}
-	.zagolovok {padding:10px;font-size:18pt;font-weight:normal;}
-	.opisanie {padding:10px}
-	#show_result {font-family:'Georgia',serif;color:#fff;border-radius:5px;padding:5px 10px;border:0;background-color:#f2354b;}
+	.modal-body{padding:20px 50px;font-size:12pt;}
+	.description{padding:30px 10px 10px;font-size:1.1rem;}
+	.answer-variants{padding:5px 0 15px 0;}
+	.result{font-size:1.3rem;text-decoration:underline;padding-right:10px;}
+	#sum_result{font-size:1.5rem;}
+	#questions{font-size:1.1rem;}
+	#answers{display:none;font-size:1.1rem;}
+	#show_result{color:#fff;border-radius:5px;padding:5px 10px;border:0;background-color:#f2354b;}
 </style>
