17555c166b9bac49ac35d0cf4cb92a15d17bc6db
iadomyshev <iadomyshev@gmail.com>
2022-02-09 23:43:11 +0300

    Choice is done

--- components/Choice.vue
+++ components/Choice.vue
@@ -4,106 +4,39 @@
       Кому подойдут консультации?
     </v-card-title>
     <v-card-text class="mt-0 custom-text-1">
-      <v-list shaped>
+      <v-list flat class="pl-15">
         <v-list-item-group
-          v-model="selectedItem"
-          color="primary"
+          color="#444"
+          no-action
         >
-
-          <v-list-item two-line>
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title>Отношения</v-list-item-title>
-              <v-list-item-subtitle> жертва-спасатель-агрессор</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-
-          <v-tooltip bottom color="red">
+          <v-tooltip
+            top
+            color="red"
+            open-delay="200"
+            content-class="custom-tooltip"
+            v-for="(item, i) in items"
+            :key="i"
+          >
             <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                color="primary"
-                dark
+              <v-list-item
+                two-line
                 v-bind="attrs"
                 v-on="on"
-                class="mt-5 py-6"
               >
-                <v-list-item
-                  two-line v-bind="attrs"
-                  v-on="on"
-                >
-                  <v-list-item-icon>
-                    <v-icon>mdi-account</v-icon>
-                  </v-list-item-icon>
-                  <v-list-item-content>
-                    <v-list-item-title>Жизнь состоит из ожиданий</v-list-item-title>
-                    <v-list-item-subtitle> и откладывания на потом</v-list-item-subtitle>
-                  </v-list-item-content>
-                </v-list-item>
-
-              </v-btn>
+                <v-list-item-icon class="pt-3">
+                  <v-icon v-text="item.icon"></v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title>{{ item.title }}</v-list-item-title>
+                  <v-list-item-subtitle>{{ item.subtitle }}</v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
             </template>
-            <span>            Вы часто ждете подходящий момент или человека.<br/>
-            Вам часто говорят - ты хороший человек, но мы не можем быть вместе.<br/>
-            Вы «торчите на эмоциях» от эйфории до эмоционального упадка.
-</span>
+            <span v-html="item.tooltip">
+            </span>
           </v-tooltip>
-
-          <v-tooltip bottom color="red">
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                color="primary"
-                dark
-                v-bind="attrs"
-                v-on="on"
-                class="mt-5 py-6"
-              >
-                <v-list-item two-line>
-                  <v-list-item-icon>
-                    <v-icon>mdi-account</v-icon>
-                  </v-list-item-icon>
-                  <v-list-item-content>
-                    <v-list-item-title>Твоей жизнью распоряжается</v-list-item-title>
-                    <v-list-item-subtitle>кто угодно, только не ты</v-list-item-subtitle>
-                  </v-list-item-content>
-                </v-list-item>
-
-              </v-btn>
-            </template>
-            <span>Для тех, кто боится сказать свое НЕТ, и живет в тревоге.<br/>
-            Кто ощущает, что живет не свою жизнь, терпит и подстраивается<br/>
-            Для тех, кто устал и хочет менять свою жизнь, но не знает, как себе помочь.</span>
-          </v-tooltip>
-
         </v-list-item-group>
       </v-list>
-      <div>
-        <div>
-          <h2></h2>
-          <p class="text-left d-none">
-            Мои консультации подойдут тем, кто устал от разрушающих отношений.<br/>
-            Всю жизнь старается, заботиться о других, в ответ упреки и безразличие.<br/>
-            Тем, кто ощущает в душе боль и пустоту одиночества.<br/>
-          </p>
-          <br/>
-        </div>
-        <div class="part">
-          <p class="text-left d-none">
-            Вы часто ждете подходящий момент или человека.<br/>
-            Вам часто говорят - ты хороший человек, но мы не можем быть вместе.<br/>
-            Вы «торчите на эмоциях» от эйфории до эмоционального упадка.
-          </p>
-          <br/>
-        </div>
-        <div class="part">
-          <p class="text-left d-none">
-            Для тех, кто боится сказать свое НЕТ, и живет в тревоге.<br/>
-            Кто ощущает, что живет не свою жизнь, терпит и подстраивается<br/>
-            Для тех, кто устал и хочет менять свою жизнь, но не знает, как себе помочь.
-          </p>
-        </div>
-      </div>
     </v-card-text>
   </v-card>
 </template>
@@ -113,16 +46,28 @@ export default {
     selectedItem: 0,
     items: [
       {
-        text: 'Отношения жертва-спасатель-агрессор',
-        icon: 'mdi-clock'
+        title: 'Отношения',
+        subtitle: 'жертва-спасатель-агрессор',
+        icon: 'mdi-human-male-female',
+        tooltip: 'Мои консультации подойдут тем, кто устал от разрушающих отношений.<br/>\n' +
+          'Всю жизнь старается, заботиться о других, в ответ упреки и безразличие.<br/>\n' +
+          'Тем, кто ощущает в душе боль и пустоту одиночества.'
       },
       {
-        text: 'Жизнь состоит из ожиданий и откладывания на потом',
-        icon: 'mdi-account'
+        title: 'Жизнь состоит из ожиданий',
+        subtitle: 'и откладывания на потом',
+        icon: 'mdi-clock-fast',
+        tooltip: 'Вы часто ждете подходящий момент или человека.<br/>\n' +
+          'Вам часто говорят - ты хороший человек, но мы не можем быть вместе.<br/>\n' +
+          'Вы «торчите на эмоциях» от эйфории до эмоционального упадка.'
       },
       {
-        text: 'Conversions',
-        icon: 'mdi-flag'
+        title: 'Твоей жизнью распоряжается',
+        subtitle: 'кто угодно, только не ты',
+        icon: 'mdi-account-minus-outline',
+        tooltip: 'Для тех, кто боится сказать свое НЕТ, и живет в тревоге.<br/>\n' +
+          'Кто ощущает, что живет не свою жизнь, терпит и подстраивается<br/>\n' +
+          'Для тех, кто устал и хочет менять свою жизнь, но не знает, как себе помочь.'
       }
     ]
   })
@@ -133,5 +78,10 @@ h2 {
   font-size: 1.1rem;
   font-weight: bold;
   text-align: left;
+}
+
+.custom-tooltip {
+  opacity: var(--v-tooltip-opacity, 1) !important;
+  background: var(--v-tooltip-bg, rgba(97, 97, 97, 1)) !important;
 }
 </style>
