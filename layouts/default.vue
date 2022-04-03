b29535bcd831ea880958604ea1386a046dcdb3af
iadomyshev <iadomyshev@gmail.com>
2022-04-03 22:54:36 +0400

    Progress linear color correction.

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
     <v-container>
       <v-tabs centered>
         <v-tab @click="$router.push('/')">Главная</v-tab>
-        <v-tab @click="$router.push('/group')">Терапевтическая группа</v-tab>
+        <v-tab @click="$router.push('/art-therapy-group')">Арт-Терапевтическая группа</v-tab>
         <v-tab @click="$router.push('/course')" class="d-none">Курс "Океан Эмоций"</v-tab>
         <v-tab @click="$router.push('/price')">Стоимость</v-tab>
         <v-tab @click="$router.push('/documents')">Подтверждение образования</v-tab>
