c3556cc63ef974056915be19913d7e6ea09fe9d3
iadomyshev <iadomyshev@gmail.com>
2022-04-06 02:53:34 +0400

    Test result page improvements. Format telegram message.

--- plugins/telegram.js
+++ plugins/telegram.js
@@ -1,15 +1,15 @@
 export default (context, inject) => {
   // This function send notification to telegram
-  const telegram = (text) => {
+  const telegram = text => {
     console.log('%c%s', 'color: green', 'Send with telegram:')
     console.log('%c%s', 'color: brown', text)
     const url = new URL('https://psysreda.ru/public/telegram')
-    const params = { text: text }
+    const params = {
+      text: text,
+      mode: localStorage.getItem('test_env') !== null ? 'test' : 'prod'
+    }
     url.search = new URLSearchParams(params).toString()
     fetch(url)
   }
-  // Inject telegram(text) in Vue, context and store.
   inject('telegram', telegram)
-  // For Nuxt <= 2.12
-  // context.telegram = telegram
 }
