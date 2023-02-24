c796809fc3b5c0f96f20bb5a1fcc53ff66315a60
iliatech <iadomyshev@gmail.com>
2023-02-24 14:29:56 +0400

    social links to array, css refactor

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,15 @@
         <router-view />
       </v-main>
 
-      <div class="empty-space"></div>
       <v-footer fixed padless>
-        <v-btn icon href="https://www.facebook.com/psysreda" target="_blank">
-          <v-icon color="#4267B2" size="24px">mdi-facebook</v-icon>
-        </v-btn>
-        <v-btn icon href="https://www.instagram.com/mypsyholog" target="_blank">
-          <v-icon color="#e95950" size="24px">mdi-instagram</v-icon>
+        <v-btn
+          v-for="social in socialNetworks"
+          :key="social.link"
+          :href="social.link"
+          target="_blank"
+          icon
+        >
+          <v-icon :color="social.color" size="24px">{{ social.icon }}</v-icon>
         </v-btn>
       </v-footer>
     </v-container>
@@ -24,7 +26,7 @@
 </template>
 
 <script>
-import { appTabs } from "@/settings";
+import { appTabs, socialNetworks } from "@/settings";
 
 export default {
   name: "App",
@@ -33,6 +35,7 @@ export default {
     return {
       menu: "",
       appTabs,
+      socialNetworks,
     };
   },
 
@@ -44,28 +47,24 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
+@import "@/assets/scss/variables.scss";
+
 .container {
-  max-width: 1240px;
+  max-width: $container-max-width;
 }
 
 .v-tabs {
-  margin-bottom: 100px;
+  margin-bottom: $space-after-tabs;
 }
 
 .v-footer {
   display: flex;
   justify-content: center;
-  height: 45px;
-  border-top: 1px dashed #aaa !important;
+  height: $footer-height;
+  border-top: $footer-border-style !important;
 }
 
-.v-card__text {
-  background: #eee;
-  padding-top: 15px;
-  padding-bottom: 15px;
-}
-
-.empty-space {
-  padding-bottom: 100px;
+::v-deep .v-main {
+  padding-bottom: $space-after-blocks !important;
 }
 </style>
--- src/settings/index.js
+++ src/settings/index.js
@@ -27,3 +27,16 @@ export const appTabs = [
     label: "Подтверждение образования",
   },
 ];
+
+export const socialNetworks = [
+  {
+    link: "https://www.facebook.com/psysreda",
+    icon: "mdi-facebook",
+    color: "#4267B2",
+  },
+  {
+    link: "https://www.instagram.com/mypsyholog",
+    icon: "mdi-instagram",
+    color: "#e95950",
+  },
+];
